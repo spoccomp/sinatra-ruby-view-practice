978e2bcf187bc008300b357abc3be5ce9a46a3f3
richard trapanese <richardtrapanese@gmail.com>
2018-09-07 10:21:13 -0400

    finished the excersie passed all tests

--- app.rb
+++ app.rb
@@ -9,3 +9,14 @@ get "/double_time" do
     @text = "Hello"
     erb :double_time
 end
+
+get "/info" do
+
+    erb :form
+end
+
+post "/form" do
+    puts "Your login: #{:login}" 
+    puts "Your password: #{:password}"
+    erb :index
+end
